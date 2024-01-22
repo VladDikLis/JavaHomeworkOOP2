d63b4be65d845c8914190b65082e21c30139fa02
VladLisitsyn <vladlisitsindiklis@yandex.ru>
2024-01-22 12:48:12 +0800

    homework

--- src/main/java/ru/otus/java/homework/animal/Animal.java
+++ src/main/java/ru/otus/java/homework/animal/Animal.java
@@ -17,5 +17,68 @@ public class Animal {
         this.stamina = stamina;
     }
 
+    public void animalInfoCondition() {
+        System.out.println("Имя животного: " + name);
+        System.out.println("Состояние: " + condition);
+        System.out.println();
+    }
 
+    public void run(Integer distance) {
+        if (Objects.equals(condition, "Устал")) {
+            System.out.println(name + " устал");
+            return;
+        }
+        int i = 0;
+        int t = 0;
+        while (distance > i) {
+            i += runningSpeed;
+            t++;
+            stamina -= 1 * runningSpeed;
+            if (stamina < 0) {
+                System.out.println(name + " устал и не смог преодолеть дистанцию");
+                condition = "Устал";
+                t = -1;
+                System.out.println("Время: " + t);
+                System.out.println();
+                return;
+            }
+        }
+        if (stamina >= 0) {
+            System.out.println(name + " преодолел дистанцию. Это был забег");
+        }
+        System.out.println("Время: " + t);
+        System.out.println();
+    }
+
+    public void swim(Integer distance) {
+        if (staminaSwimmingWaste == 0) {
+            System.out.println(name + " не умеет плавать");
+            System.out.println();
+            return;
+        }
+        if (Objects.equals(condition, "Устал")) {
+            System.out.println(name + " устал");
+            return;
+        }
+        int i = 0;
+        int t = 0;
+        while (distance > i) {
+            i += swimmingSpeed;
+            t++;
+            stamina -= staminaSwimmingWaste * swimmingSpeed;
+            if (stamina < 0) {
+                System.out.println(name + " устал и не смог преодолеть дистанцию");
+                condition = "Устал";
+                t = -1;
+                System.out.println("Время: " + t);
+                System.out.println();
+                return;
+            }
+        }
+        if (stamina >= 0) {
+            System.out.println(name + " преодолел дистанцию. Это был заплыв");
+        }
+        System.out.println("Время: " + t);
+        System.out.println();
+    }
 }
--- src/main/java/ru/otus/java/homework/animal/Horse.java
+++ src/main/java/ru/otus/java/homework/animal/Horse.java
@@ -2,5 +2,9 @@ package ru.otus.java.homework.animal;
 
 public class Horse extends Animal {
 
-
+    public Horse(String name, Integer runningSpeed, Integer swimmingSpeed, Integer stamina) {
+        super(name, runningSpeed, swimmingSpeed, stamina);
+        this.condition = "Полон сил";
+        this.staminaSwimmingWaste = 4;
+    }
 }
